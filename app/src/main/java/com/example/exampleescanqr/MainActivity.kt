839f4a45cfd05e4a797500e26c92d528e4e59b60
Eduar Rugamas <juaneduardo021299@hotmail.com>
2020-09-23 14:10:28 -0600

    Terminacion de code

--- app/src/main/java/com/example/exampleescanqr/MainActivity.kt
+++ app/src/main/java/com/example/exampleescanqr/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.example.exampleescanqr
 
+import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.SurfaceHolder
-
 import android.view.SurfaceHolder.Callback
 import android.view.SurfaceView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.Detector.Detections
 import com.google.android.gms.vision.barcode.Barcode
 import com.google.android.gms.vision.barcode.BarcodeDetector
+import java.io.IOException
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     private var barcodeDetector : BarcodeDetector? = null
     private var cameraSource : CameraSource? = null
     private var cameraSurfaceView : SurfaceView? = null
+    private var qrResult: TextView? = null
 
 
 
@@ -28,16 +36,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             if (Build.VERSION.SDK_INT >= 23){
-                if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA))
+                if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA))
                     Toast.makeText(this, "Esta app requiere permiso de camara", Toast.LENGTH_LONG).show()
-                    requestPermissions(arrayOf(android.Manifest.permission.CAMERA), MY_PERMISSION_CAMERA)
+                    requestPermissions(arrayOf(Manifest.permission.CAMERA), MY_PERMISSION_CAMERA)
             }
         }
 
             //texto con el resultado qr
-
+                qrResult = findViewById(R.id.qr_result)
             //vista de la camara
             cameraSurfaceView = findViewById(R.id.camera_view)
 
@@ -49,20 +57,51 @@ class MainActivity : AppCompatActivity() {
 
             //prepara el lector de qr
             cameraSurfaceView?.holder?.addCallback(object: Callback{
-                override fun surfaceCreated(p0: SurfaceHolder) {
-                    TODO("Not yet implemented")
+
+                //verificar si existen los permisos dados
+                override fun surfaceCreated(holder: SurfaceHolder) {
+                    if (ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+                    {
+                        try {
+                            cameraSource?.start(cameraSurfaceView?.holder)
+                        }catch (e: IOException){
+                           Log.e("Error", e.message.toString())
+                        }
+                    }
                 }
 
-                override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
-                    TODO("Not yet implemented")
-                }
+                override fun surfaceChanged(holder: SurfaceHolder, p1: Int, p2: Int, p3: Int) {}
 
-                override fun surfaceDestroyed(p0: SurfaceHolder) {
-                    TODO("Not yet implemented")
+                override fun surfaceDestroyed(holder: SurfaceHolder) {
+                    cameraSource?.stop()
                 }
 
             })
-        }
 
-    
+        barcodeDetector?.setProcessor(object : Detector.Processor<Barcode> {
+            override fun release() {}
+
+            override fun receiveDetections(detections: Detections<Barcode>?) {
+                val barcodes = detections?.detectedItems
+
+                if (barcodes!!.size() != 0){
+                    qr_result.post( Runnable {
+                            fun run () {
+                                qr_result.text = (barcodes.valueAt(0).displayValue.toString())
+                            }
+                    })
+
+                    barcodeDetector?.release()
+
+                }
+            }
+
+        })
+
+
+    }
 }
+
+
+
+
