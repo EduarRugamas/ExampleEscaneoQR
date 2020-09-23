700a34b3f8b22dad7be88337fb305f57b8bda432
Eduar Rugamas <juaneduardo021299@hotmail.com>
2020-09-23 11:31:11 -0600

    Escaner de qr y barcode, permission and code of camera and qr

---
+++ app/src/main/java/com/example/exampleescanqr/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.exampleescanqr
+
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.SurfaceView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val MY_PERMISSION_CAMERA: Int = 100
+    private var barcodeDetector : BarcodeDetector? = null
+    private var cameraSource : CameraSource? = null
+    private var cameraSurfaceView : SurfaceView? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= 23){
+                if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA))
+                    Toast.makeText(this, "Esta app requiere permiso de camara", Toast.LENGTH_LONG).show()
+                    requestPermissions(arrayOf(android.Manifest.permission.CAMERA), MY_PERMISSION_CAMERA)
+            }
+        }
+
+            //texto con el resultado qr
+
+            //vista de la camara
+            cameraSurfaceView = findViewById(R.id.camera_view)
+
+            //creacion de lector qr
+            barcodeDetector = BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build()
+
+            //creacion de camara
+            cameraSource = CameraSource.Builder(this, barcodeDetector).build()
+
+            
+
+    }
+}
